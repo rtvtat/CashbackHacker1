a4ed02871465ce3290b69be40b9de3dff147ad5f
Зайцева Татьяна <rtvtat@rambler.ru>
2022-07-20 20:55:21 +0300

    Add changes in testng

--- src/test/java/ru/netology/service/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -8,17 +8,19 @@ public class CashbackHackServiceTest {
 
     @Test
     public void testRemain() {
-CashbackHackService service = new CashbackHackService();
-int amount = 500;
-int actual = service.remain(amount);
+        CashbackHackService service = new CashbackHackService();
+        int amount = 500;
+        int actual = service.remain(amount);
         int expected = 500;
+        assertEquals(actual, expected);
     }
 
     @Test
-    public void testRemain() {
+    public void noTestRemain() {
         CashbackHackService service = new CashbackHackService();
         int amount = 1000;
         int actual = service.remain(amount);
         int expected = 0;
+        assertEquals(actual, expected);
     }
 }
